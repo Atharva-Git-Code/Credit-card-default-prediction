e34609038f401f3251c03a2110ecd76382f3adb0
Atharva-Git-Code <92671125+Atharva-Git-Code@users.noreply.github.com>
2023-04-20 15:52:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,68 +1,108 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+import streamlit as st
 import pickle
-import bz2file as bz2
+import numpy as np
 
-app = Flask(__name__)
-def decompress_pickle(file):
-  data = bz2.BZ2File(file, 'rb')
-  data = pickle.load(data)
-  return data
+# import the model
+pipe = pickle.load(open('ccdp.pkl','rb'))
+df = pickle.load(open('ccdp.pbz2','rb'))
 
-model = decompress_pickle('ccdp.pbz2')
+st.title("Credit Card Default Prediction")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+#Limited balance
+LIMIT_BAL = st.text_input('LIMIT_BAL')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    if request.method == 'POST':
-        #gender = gender_encode(int(request.form['gender']))
-        #education = education_encode(int(request.form['education']))
-        #marital_status = marital_encode(int(request.form['marriage']))
-        age = [int(request.form['age'])]
-        bal_limit = [int(request.form['limit_bal'])]
-        rs_6 = [int(request.form['april_rs'])]
-        rs_5 = [int(request.form['may_rs'])]
-        rs_4 = [int(request.form['june_rs'])]
-        rs_3 = [int(request.form['july_rs'])]
-        rs_2 = [int(request.form['august_rs'])]
-        rs_1 = [int(request.form['september_rs'])]
-        bill_6 = [int(request.form['bill_amt6'])]
-        bill_5 = [int(request.form['bill_amt5'])]
-        bill_4 = [int(request.form['bill_amt4'])]
-        bill_3 = [int(request.form['bill_amt3'])]
-        bill_2 = [int(request.form['bill_amt2'])]
-        bill_1 = [int(request.form['bill_amt1'])]
-        pay_6 = [int(request.form['pay_amt6'])]
-        pay_5 = [int(request.form['pay_amt5'])]
-        pay_4 = [int(request.form['pay_amt4'])]
-        pay_3 = [int(request.form['pay_amt3'])]
-        pay_2 = [int(request.form['pay_amt2'])]
-        pay_1 = [int(request.form['pay_amt1'])]
 
-    bill_amt_avg = [round(np.mean([bill_6, bill_5, bill_4, bill_3, bill_2, bill_1]), 2)]
-    features = rs_1 + rs_2 + pay_1 + bill_1
-    features = features + bal_limit + age + pay_2 + bill_2
-    features = features + pay_3 + bill_3 + bill_4 + pay_4
-    features = features + pay_6 + bill_5 + bill_6 + bill_amt_avg
+#sex
+SEX = st.selectbox('SEX',['Male','Female'])
+
+#Education
+EDUCATION = st.selectbox('EDUCATION', ['graduate school' , 'university' , 'high school' , 'others' , 'unknown', 'unknown'])
+
+#Marriage
+MARRIAGE = st.selectbox('MARRIAGE', ['married', 'single' , 'others']) 
+
+# Age
+AGE = st.number_input('Age of person')
+
+#pay
+
+PAY_0 = st.selectbox('PAY_0',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+PAY_2 = st.selectbox('PAY_2',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+PAY_3 = st.selectbox('PAY_3',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+PAY_4 = st.selectbox('PAY_4',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+PAY_5 = st.selectbox('PAY_5',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+PAY_6 = st.selectbox('PAY_6',[-2,-1,1,2,3,4,5,6,7,8,9,0])
+
+#bill
+BILL_AMT1 =st.number_input('bill statement in September')
+BILL_AMT2 =st.number_input('bill statement in August')
+BILL_AMT3 =st.number_input('bill statement in July')
+BILL_AMT4 =st.number_input('bill statement in June')
+BILL_AMT5 =st.number_input('bill statement in May')
+BILL_AMT6 =st.number_input('bill statement in April')
+
+
+#Pay
+PAY_AMT1 =st.number_input('previous payment in September')
+PAY_AMT2 =st.number_input('previous payment in August')
+PAY_AMT3 =st.number_input('previous payment in July')
+PAY_AMT4 =st.number_input('previous payment in June')
+PAY_AMT5 =st.number_input('previous payment in May')
+PAY_AMT6 =st.number_input('previous payment in April')
+
+
+
+
+if st.button('PREDICT CREDIT CARD DEFAULTER PAYMENT'):
+    # query
     
-    features_arr = [np.array(features)]
-
-    prediction = model.predict(features_arr)
-
-    #result = ""
-    #if prediction == 1:
-    #  result = "This customer IS LIKELY TO DEFAULT next month."
-    #else:
-    #  result = "This customer IS NOT LIKELY TO DEFAULT next month."
+    if SEX == 'Male':
+        SEX = 1
+    else:
+        SEX = 2
 
 
-    return render_template('index.html', prediction=prediction)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if EDUCATION == 'graduate school':
+        EDUCATION = 1
+    elif EDUCATION == 'university':
+        EDUCATION = 2
+    elif EDUCATION == 'high school':
+        EDUCATION = 3
+    elif EDUCATION == 'others':
+        EDUCATION = 4
+    elif EDUCATION == 'unknown':
+        EDUCATION = 5
+    else:
+        EDUCATION =6
+        
+
+    if MARRIAGE == 'married':
+        MARRIAGE = 1
+    elif MARRIAGE == 'single':
+        MARRIAGE = 2
+    else:
+        MARRIAGE = 3
+
+    
+    query1 = np.array([LIMIT_BAL, SEX, EDUCATION, MARRIAGE, AGE, PAY_0, PAY_2,
+       PAY_3, PAY_4, PAY_5, PAY_6, BILL_AMT1, BILL_AMT2,
+       BILL_AMT3, BILL_AMT4, BILL_AMT5, BILL_AMT6, PAY_AMT1,
+       PAY_AMT2, PAY_AMT3, PAY_AMT4, PAY_AMT5, PAY_AMT6])
+
+
+
+    query = np.array(query1).astype(float).reshape(1,-1)
+    #query = query.reshape(1,23)
+    final = pipe.predict(query)
+
+    #st.title("The predicted price of this configuration is " + final)
+
+    print(final)
+
+    if (final == [1]):
+        st.header("The credit card holder will be Defaulter in the next month")
+    else:
+        st.header("The Credit card holder will not be Defaulter in the next month")
+
+
